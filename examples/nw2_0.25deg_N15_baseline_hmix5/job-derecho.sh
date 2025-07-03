568a6cf56315fa4f0404d52983c799ce6b8a8653
alperaltuntas <alperaltuntas@gmail.com>
2025-07-03 14:56:41 -0600

    Add neverworld example

---
+++ examples/nw2_0.25deg_N15_baseline_hmix5/job-derecho.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#PBS -N mom6_standalone
+#PBS -A <PROJECT_CODE>
+#PBS -j oe
+#PBS -k eod
+#PBS -q main
+#PBS -l walltime=00:15:00
+#PBS -l select=6:ncpus=128:mpiprocs=128
+#PBS -l job_priority=premium
+
+### Set temp to scratch
+export TMPDIR=${SCRATCH}/${USER}/temp && mkdir -p $TMPDIR
+
+COMPILER=intel
+
+mpiexec ../../bin/${COMPILER}/MOM6/MOM6
