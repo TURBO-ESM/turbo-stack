0490e1db0a91309d5bd1db8ef8712e5eb1e44704
Hilario Torres <hilario.c.torres@gmail.com>
2025-07-22 16:22:54 -0700

    Starting point for tripolar grid dev test, main.cpp provided by https://github.com/WeiqunZhang/amrex-devtests/tree/main/tripolar. Added spack environment for installing dependencies. Changed to a CMake build the application.

---
+++ src/development_tests/amrex/tripolar_grid/README.md
@@ -0,0 +1,15 @@
+# Tripolar Grid using AMReX Development Test
+
+A minimal working example implementing the communication pattern needed for tripolar girds in AMReX.
+
+## Prerequisites
+
+- The environment variable `TURBO_STACK_ROOT` should point to the directory where you cloned the [turbo-stack repository](https://github.com/TURBO-ESM/turbo-stack/).
+- [spack](https://spack.io/)
+
+## Build and Run
+If you have spack installed on you system then the script build_and_run.sh should be all you need to get started. That scirpt:
+- installs all other dependencies (cmake, amrex, etc...) in a spack environment 
+- activates that spack environmet
+- builds the tripolar grid amrex example using cmake
+- runs it
---
+++ src/development_tests/amrex/tripolar_grid/build_and_run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+###############################################################################
+# User Input
+###############################################################################
+
+# Set the where the build directory will be created. You can change this to any location you prefer.
+export BUILD_DIR=~/tripolar_grid_with_amrex_build
+
+# You can also set the DEBUG environment variable to 1 to enable debugging features.
+if [[ "${DEBUG:-0}" == "1" ]]; then
+    set -e  # Exit immediately if a command exits with a non-zero status
+    set -u  # Treat expanding empty variables as an error
+    set -x  # Print each command before executing it
+fi
+
+
+###############################################################################
+# Check Pre-requisites
+###############################################################################
+
+if [[ -z "$TURBO_STACK_ROOT" || ! -d "$TURBO_STACK_ROOT" ]]; then
+    echo "Error: TURBO_STACK_ROOT environment variable is not set or does not point to a valid directory. It should point to where you cloned the turbo-stack repository." >&2
+    exit 1
+fi
+
+if ! command -v spack &> /dev/null; then
+    echo "Error: spack command not found. Please load Spack before running this script." >&2
+    exit 1
+fi
+
+
+###############################################################################
+# Spack Environment Setup
+###############################################################################
+
+spack_environment_name="tripolar_grid_amrex"
+spack_environment_config_file=$TURBO_STACK_ROOT/src/development_tests/amrex/tripolar_grid/spack.yaml
+
+if ! spack env list | grep --word-regexp --quiet "$spack_environment_name"; then
+    spack env create "$spack_environment_name" "$spack_environment_config_file"
+fi
+
+spack env activate $spack_environment_name 
+
+spack install
+
+
+###############################################################################
+# Build and Run the Code
+###############################################################################
+
+# Generate the build directory. 
+cmake -S $TURBO_STACK_ROOT/src/development_tests/amrex/tripolar_grid -B $BUILD_DIR --fresh
+
+# Build the code. 
+cmake --build $BUILD_DIR
+
+# Run the code. 
+$BUILD_DIR/tripolar_grid
