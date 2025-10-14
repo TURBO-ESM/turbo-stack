7a42f5a7c68f45c9547fd9421e7b6d5f60fc12fd
Hilario Torres <hilario.c.torres@gmail.com>
2025-10-14 14:26:11 -0600

    Move compiler to be regular package.

--- src/development_tests/amrex/tripolar_grid/build_and_run.sh
+++ src/development_tests/amrex/tripolar_grid/build_and_run.sh
@@ -167,6 +167,7 @@ if [[ "$machine" == "derecho" ]]; then
     spack external find hdf5
     spack external find mpich
 elif [[ "$machine" == "ci_container" ]]; then
+    spack external find gcc
     spack external find cmake
     spack external find hdf5
     spack external find openmpi
