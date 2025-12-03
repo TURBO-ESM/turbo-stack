97e13cad1e0c5fff8f06d0bd7dbc92cfa4adc2ec
Michael Waxmonsky <mwaxmonsky@ucar.edu>
2025-12-03 10:25:28 -0700

    Converting AMReX to a submodule.

--- build.sh
+++ build.sh
@@ -245,15 +245,12 @@ fi
 
 # Check if AMREX_PATH was provided or if need to clone and build first
 if [[ "${INFRA}" == "TIM" && ! -v ${AMREX_PATH} ]]; then
-  echo "Path to AMReX not declared.  Cloning and building AMReX"
-  if [[ ! -d "${ROOTDIR}/submodules/amrex" ]]; then
-    git clone -b 25.11 https://github.com/AMReX-Codes/amrex.git "${ROOTDIR}/submodules/amrex"
-    cd "${ROOTDIR}/submodules/amrex"
-    cmake -B./build -S. -DAMReX_FORTRAN_INTERFACES=YES -DAMReX_FORTRAN=YES -DCMAKE_INSTALL_PREFIX=./install
-    cd build
-    make
-    make install
-  fi
+  echo "Path to AMReX not declared.  Building AMReX through submodule."
+  cd "${ROOTDIR}/submodules/amrex"
+  cmake -B./build -S. -DAMReX_FORTRAN_INTERFACES=YES -DAMReX_FORTRAN=YES -DCMAKE_INSTALL_PREFIX=./install
+  cd build
+  make
+  make install
   AMREX_PATH="${ROOTDIR}/submodules/amrex/install"
 fi
 
