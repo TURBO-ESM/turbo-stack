38878813567efa2ae9586781b573ed041305e0cd
alperaltuntas <alperaltuntas@gmail.com>
2026-05-19 22:54:24 -0600

    match compile-time environment in gpu job submission script

--- examples/double_gyre/job-gpu.sh
+++ examples/double_gyre/job-gpu.sh
@@ -6,7 +6,7 @@
 #PBS -l select=1:ncpus=4:mpiprocs=4:ngpus=4
 
 # Load modules to match compile-time environment
-module load cuda/12.9.0 nvhpc/25.9 cray-mpich/8.1.32
+module load ncarenv/25.10 cuda/12.9.0 hdf5/1.14.6 nvhpc/25.9 ncarcompilers/1.1.0 netcdf/4.9.3
 
 ### Set temp to scratch
 export TMPDIR=${SCRATCH}/${USER}/temp && mkdir -p $TMPDIR
