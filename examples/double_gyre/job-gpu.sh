cbdcdcf751f6145a1f7c93e312757deabfe9f100
alperaltuntas <alperaltuntas@gmail.com>
2026-05-19 22:30:53 -0600

    update double_gyre gpu job submission script to use 4 GPUs and 4 GPUs.

--- examples/double_gyre/job-gpu.sh
+++ examples/double_gyre/job-gpu.sh
@@ -3,7 +3,7 @@
 #PBS -N mom6_standalone_gpu
 #PBS -q main
 #PBS -l walltime=00:03:00
-#PBS -l select=1:ncpus=16:mpiprocs=2:ngpus=2
+#PBS -l select=1:ncpus=4:mpiprocs=4:ngpus=4
 
 # Load modules to match compile-time environment
 module load cuda/12.9.0 nvhpc/25.9 cray-mpich/8.1.32
@@ -12,10 +12,6 @@ module load cuda/12.9.0 nvhpc/25.9 cray-mpich/8.1.32
 export TMPDIR=${SCRATCH}/${USER}/temp && mkdir -p $TMPDIR
 
 COMPILER=nvhpc
-INFRA=FMS2
+INFRA=TIM
 
-# Serial version works:
-../../bin/${COMPILER}/MOM6_using_${INFRA}/MOM6/MOM6
-
-# # Multiple ranks with GPU offloading fails at day 2.
-# mpiexec -n 2 -ppn 2 set_gpu_rank ../../bin/${COMPILER}/MOM6_using_${INFRA}/MOM6/MOM6
+mpiexec -n 4 -ppn 4 set_gpu_rank ../../bin/${COMPILER}/MOM6_using_${INFRA}/MOM6/MOM6
